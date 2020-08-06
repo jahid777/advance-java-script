9d13a97a564da70b99c49b754a77f54fe55dd97f
jahid777 <66377960+jahid777@users.noreply.github.com>
2020-08-07 02:49:43 +0600

    duble and truple equal condition check

---
+++ duble-vs-triple-equal.js
@@ -0,0 +1,39 @@
+//true condition
+const first = 1;
+const second =  true;
+if(first == second){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+//true condition
+const first = 0;
+const second =  false;
+if(first == second){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+
+//false condition
+const first = 2;
+const second =  "2";
+if(first === second){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+
+//true condition
+const first = 2;
+const second =  "2";
+if(first == second){
+    console.log("this is true");
+}
+else{
+    console.log("this is false");
+}
+
