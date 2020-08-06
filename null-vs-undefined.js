3b7bbabf864c43e468d6791f1384757e2dc2dd84
jahid777 <66377960+jahid777@users.noreply.github.com>
2020-08-07 02:18:36 +0600

    null vs undefined for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let name;
+
+function add (num1, num2){
+    console(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1);
+}
+
+
+const student = {name: "justin", id: 1, university: "DU"};
+console.log(student.girlfriendName);
+
+let fun = undefined;
+console.log(fun);
+
+let ages =[10, 13, 14, 20, 21, 30];
+console.log(ages[25]);
