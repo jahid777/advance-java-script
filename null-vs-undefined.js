9ad4246ad8b90585bff71b782b01d42eafc54986
jahid777 <66377960+jahid777@users.noreply.github.com>
2020-08-07 20:28:28 +0600

    null and undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,22 +1,22 @@
-// //how can we get undefined
+//how can we get undefined
 
-// let name;
+let name;
 
-// function add (num1, num2){
-//     console(num1 + num2);
-//     return
-// }
+function add (num1, num2){
+    console(num1 + num2);
+    return
+}
 
-// function add(num1, num2){
-//     console.log(num1);
-// }
+function add(num1, num2){
+    console.log(num1);
+}
 
-// const student = {name: "justin", id: 1, university: "DU"};
-// console.log(student.girlfriendName);
+const student = {name: "justin", id: 1, university: "DU"};
+console.log(student.girlfriendName);
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;
+console.log(fun);
 
-// let ages =[10, 13, 14, 20, 21, 30];
-// console.log(ages[25]);
+let ages =[10, 13, 14, 20, 21, 30];
+console.log(ages[80]);
 
