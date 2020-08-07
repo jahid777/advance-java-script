0a06ea4f4855d4c5d6ad5b9916ead811b37a9ae7
jahid777 <66377960+jahid777@users.noreply.github.com>
2020-08-07 14:35:05 +0600

    currection

--- map-filter.js
+++ map-filter.js
@@ -1,48 +1,53 @@
-// const numbers = [3, 4, 5, 6, 7, 8];
-// const output =[];
-// for(let i=0; i<numbers.length; i++){
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);   
-// }
+//this is for loop with squire 
+const numbers = [3, 4, 5, 6, 7, 8];
+const output =[];
+for(let i = 0; i < numbers.length; i++ ){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);
 
-// const numbers = [3, 4, 5, 6, 7, 8];
-// function squire(element){
-//     return element * element;
-// }
-// const output = squire(5);
-// console.log(output);
+
+//this is function for squire
+const numbers = [3, 4, 5, 6, 7, 8];
+function squire(element){
+    return element * element;
+}
+const output = squire(5);
+console.log(output);
 
 //this is squire with arrow 
-// const squire = element => element * element;
-// const result = squire(5);
-// console.log(result);
-// const squire = x => x * x;
-// const result = squire(5);
-// console.log(result);
+const squire = element => element * element;
+const result = squire(5);
+console.log(result);
+const squire = x => x * x;
+const result = squire(5);
+console.log(result);
 
+//this is map with three things
+const numbers = [3, 4, 5, 6, 7, 8];
+numbers.map(function(element, index, array){
+    console.log(element, index, array);
+})
 
-// const numbers = [3, 4, 5, 6, 7, 8];
-// numbers.map(function(element, index, array){
-//     console.log(element, index, array);
-// })
-
-// const numbers = [3, 4, 5, 6, 7, 8];
-//  const result = numbers.map(function(element){
-//     return element * element ;
-// })
-// console.log(result);
+//this is map with squire
+const numbers = [3, 4, 5, 6, 7, 8];
+ const result = numbers.map(function(element){
+    return element * element ;
+})
+console.log(result);
 
 
 //this is map and arrow for squire
-// const numbers = [3, 4, 5, 6, 7, 8];
-// const result = numbers.map(x => x*x);
-// console.log(result);
+const numbers = [3, 4, 5, 6, 7, 8];
+const result = numbers.map(x => x*x);
+console.log(result);
 
 //this is filter with large number
-// const numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const bigNumber = numbers.filter(x => x > 5);
-// console.log(bigNumber);
+const numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const bigNumber = numbers.filter(x => x > 5);
+console.log(bigNumber);
 
 //this is find with large number
 const numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
