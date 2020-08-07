63fbf25e2500314c4db4bf405dfce72ab66dd761
jahid777 <66377960+jahid777@users.noreply.github.com>
2020-08-07 18:32:36 +0600

    currection

--- object-property.js
+++ object-property.js
@@ -1,38 +1,38 @@
 //this is find name using by for loop
-// const student =[
-//     {id:1, name: "shakib"},
-//     {id:2, name: "shuchorita"},
-//     {id:3, name: "mahi"},
-//     {id:4, name: "mostofa"}
-// ];
-// const output =[];
-// for(let i=0; i < student.length; i++){
-//     const element = student[i];
-//     const result = element.name;
-//      output.push(result);
-// } 
-// console.log(output);
+const student =[
+    {id:1, name: "shakib"},
+    {id:2, name: "shuchorita"},
+    {id:3, name: "mahi"},
+    {id:4, name: "mostofa"}
+];
+const output =[];
+for(let i=0; i < student.length; i++){
+    const element = student[i];
+    const result = element.name;
+     output.push(result);
+} 
+console.log(output);
 
 //find the name using the map 
-// const student =[
-//     {id:1, name: "shakib"},
-//     {id:2, name: "shuchorita"},
-//     {id:3, name: "mahi"},
-//     {id:4, name: "mostofa"}
-// ];
-// const name = student.map(s => s.name);
-// console.log(name);
+const student =[
+    {id:1, name: "shakib"},
+    {id:2, name: "shuchorita"},
+    {id:3, name: "mahi"},
+    {id:4, name: "mostofa"}
+];
+const name = student.map(s => s.name);
+console.log(name);
 
 
 //find the id from the information
-// const student =[
-//     {id:1, name: "shakib"},
-//     {id:2, name: "shuchorita"},
-//     {id:3, name: "mahi"},
-//     {id:4, name: "mostofa"}
-// ];
-// const ids = student.map(s => s.id);
-// console.log(ids);
+const student =[
+    {id:1, name: "shakib"},
+    {id:2, name: "shuchorita"},
+    {id:3, name: "mahi"},
+    {id:4, name: "mostofa"}
+];
+const ids = student.map(s => s.id);
+console.log(ids);
 
 
 //find the big id from the information by using filter
